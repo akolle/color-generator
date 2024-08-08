baf53d3cdc2dfbb134a4021306a8565092f8f80b
akolle <andykoller55@yahoo.com>
2024-08-08 13:41:25 -0400

    adding values.js functionality and setting up state for that

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import ColorList from './Components/ColorList'
 import Form from './Components/Form'
 import SingleColor from './Components/SingleColor'
+import Values from 'values.js'
 
 const App = () => {
+  const [colors, setColors] = useState([new Values('#f15025').all(10)])
+
   return (
     <div>
       <Form />
