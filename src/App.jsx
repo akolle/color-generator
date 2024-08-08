44b6e2a37245435c45203a3f7d16db9d859bc6b3
akolle <andykoller55@yahoo.com>
2024-08-08 18:54:11 -0400

    using toastify for display messages. added singlecolor functionality and colorlist

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import ColorList from './Components/ColorList'
 import Form from './Components/Form'
 import SingleColor from './Components/SingleColor'
 import Values from 'values.js'
+import { ToastContainer, toast } from 'react-toastify'
 
 const App = () => {
-  const [colors, setColors] = useState([new Values('#f15025').all(10)])
+  const [colors, setColors] = useState(new Values('#f15025').all(10))
+
+  toast.error('error')
+  toast.success('success')
 
   return (
     <div>
       <Form />
-      <ColorList />
-      <SingleColor />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </div>
   )
 }
